6fed0999b7aa0feb9488088fa3db2cc77ec62eff
ksm0207 <ksm03071@naver.com>
2021-09-13 14:38:55 +0900

    - 유효성검사 연습

---
+++ 0913_JS/WebContent/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// 변수 선언과 초기화.
+// 자바스크립트는 내부적으로 자료형이 존재하며 값이 '할당' 될 때 자동으로 지정됨.
+
+let a1 = 100;
+
+let a2 = 30;
+
+let str = "값";
+
+let res = a1%a2;
+
+document.write(str+(a1+a2));
+document.write('<br>');
+
+document.write('<strong> '+a1+a2+' </strong>');
+
