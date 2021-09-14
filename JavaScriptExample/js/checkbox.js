ad610bb8557f24b390d1146b6b166cd31ffef109
ksm0207 <ksm03071@naver.com>
2021-09-15 07:14:54 +0900

    - 체크박스 전체선택 / 개별선택

---
+++ JavaScriptExample/js/checkbox.js
@@ -0,0 +1,33 @@
+$(function(){
+    console.log("ready");
+
+    $(".checkbox_group").on("click","#check_all",function(){
+        console.log("on() : ready ! ");
+        
+        let checked = $(this).is(":checked");
+        console.log(` this : checkbox_group :  ${checked}`);
+        /* children() : 선택한 요소의 자식(child) 요소를 모두 선택한다..*/
+        
+        /* 전체선택이 체크가 되어있는지 if문으로 구별하기.*/
+        if (checked) {
+           $(".checkbox_group").find("input").prop("checked",true);
+        
+        }else{
+            $(".checkbox_group").find("input").prop("checked",false);
+        }
+
+    }); // end of checkbox_group function
+
+    $(".checkbox_group").on("click",".normal", function(){
+
+        let check = true;
+
+        $(".normal").each(function(){
+            check = check && $(this).is(":checked");
+            console.log(check);
+        });
+        console.log("Line 29");
+        $("#check_all").prop("checked",check);
+
+    });
+});
